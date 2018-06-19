08281c32ba49b115b2fa821d4a01f848880e4d55
Igor Opaniuk <igor.opaniuk@linaro.org>
2018-06-19 09:38:05 +0300

    core: pta: introduce pTA to access LTC ASN.1 parser
    This pTA is used for:
    * Encoding/decoding x509 certificates using LTC ASN.1 parser
    * Generation of EC/RSA root attestation key-pairs/certificates (currently there
    is no support of key "injection" during factory provisioning, so basically
    certificates are generated during the first Keymaster TA invocation).
    * Generation of EC/RSA attestation certificate for the specific key.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@linaro.org>
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- core/pta/sub.mk
+++ core/pta/sub.mk
@@ -1,3 +1,126 @@
+cppflags-$(CFG_ASN1_PARSER) += -DARGTYPE=4  # Make LTC_ARGCHK() return on error
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_CLEAN_STACK -DLTC_NO_TEST -DLTC_NO_PROTOTYPES
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_TABLES -DLTC_HASH_HELPERS
+cppflags-$(_CFG_CORE_LTC_SIZE_OPTIMIZATION) += -DLTC_SMALL_CODE
+
+# https://github.com/OP-TEE/optee_os/issues/3290
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_SOURCE
+#cppflags-$(CFG_ASN1_PARSER) += -DLTC_VERBOSE
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_CIPHERS
+
+ifneq (,$(filter y,$(_CFG_CORE_LTC_AES) $(_CFG_CORE_LTC_AES_DESC)))
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_RIJNDAEL
+endif
+ifeq ($(_CFG_CORE_LTC_DES),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_DES
+endif
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_MODES
+
+ifeq ($(_CFG_CORE_LTC_ECB),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECB_MODE
+endif
+ifeq ($(_CFG_CORE_LTC_CBC),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_CBC_MODE
+endif
+ifeq ($(_CFG_CORE_LTC_CTR),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_CTR_MODE
+endif
+ifeq ($(_CFG_CORE_LTC_XTS),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_XTS_MODE
+endif
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_HASHES
+
+ifeq ($(_CFG_CORE_LTC_MD5),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_MD5
+endif
+ifeq ($(_CFG_CORE_LTC_SHA1),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA1
+endif
+ifeq ($(_CFG_CORE_LTC_SHA1_ARM32_CE),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA1_ARM32_CE
+endif
+ifeq ($(_CFG_CORE_LTC_SHA1_ARM64_CE),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA1_ARM64_CE
+endif
+ifeq ($(_CFG_CORE_LTC_SHA224),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA224
+endif
+ifneq (,$(filter y,$(_CFG_CORE_LTC_SHA256) $(_CFG_CORE_LTC_SHA256_DESC)))
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA256
+endif
+ifeq ($(_CFG_CORE_LTC_SHA256_ARM32_CE),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA256_ARM32_CE
+endif
+ifeq ($(_CFG_CORE_LTC_SHA256_ARM64_CE),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA256_ARM64_CE
+endif
+ifneq (,$(filter y,$(_CFG_CORE_LTC_SHA384) $(_CFG_CORE_LTC_SHA384_DESC)))
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA384
+endif
+ifneq (,$(filter y,$(_CFG_CORE_LTC_SHA512) $(_CFG_CORE_LTC_SHA512_DESC)))
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA512
+endif
+ifeq ($(_CFG_CORE_LTC_SHA512_256),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_SHA512_256
+endif
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_MACS
+
+ifeq ($(_CFG_CORE_LTC_HMAC),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_HMAC
+endif
+ifeq ($(_CFG_CORE_LTC_CMAC),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_OMAC
+endif
+ifeq ($(_CFG_CORE_LTC_CCM),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_CCM_MODE
+endif
+ifeq ($(_CFG_CORE_LTC_GCM),y)
+	cppflags-$(CFG_ASN1_PARSER) += -DLTC_GCM_MODE
+endif
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_PK
+
+ifeq ($(_CFG_CORE_LTC_RSA),y)
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_MRSA
+endif
+ifeq ($(_CFG_CORE_LTC_DSA),y)
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_MDSA
+endif
+ifeq ($(_CFG_CORE_LTC_DH),y)
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_MDH
+endif
+ifeq ($(_CFG_CORE_LTC_ECC),y)
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_MECC
+
+   # use Shamir's trick for point mul (speeds up signature verification)
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECC_SHAMIR
+
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECC192
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECC224
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECC256
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECC384
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_ECC521
+
+   # ECC 521 bits is the max supported key size
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_MAX_ECC=521
+endif
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_PKCS
+
+ifneq (,$(filter y,$(_CFG_CORE_LTC_RSA) $(_CFG_CORE_LTC_DSA) $(_CFG_CORE_LTC_ECC) $(_CFG_CORE_LTC_HASH)))
+   cppflags-$(CFG_ASN1_PARSER) += -DLTC_DER
+endif
+
+cppflags-$(CFG_ASN1_PARSER) += -DLTC_NO_PRNGS -DLTC_FORTUNA
+
+cppflags-$(CFG_ASN1_PARSER) += -Ilib/libmpa
+
+######
+
 subdirs-$(CFG_TEE_CORE_EMBED_INTERNAL_TESTS) += tests
 
 srcs-$(CFG_TEE_BENCHMARK) += benchmark.c
@@ -11,3 +134,7 @@ srcs-$(CFG_WITH_STATS) += stats.c
 srcs-$(CFG_SYSTEM_PTA) += system.c
 
 subdirs-y += bcm
+
+srcs-$(CFG_ASN1_PARSER) += asn1_parser.c
+srcs-$(CFG_ASN1_PARSER) += attestations.c
+srcs-$(CFG_ASN1_PARSER) += encoders.c
--- mk/config.mk
+++ mk/config.mk
@@ -479,6 +479,9 @@ CFG_SYSTEM_PTA ?= y
 # world OS.
 CFG_DEVICE_ENUM_PTA ?= y
 
+# Expose ASN1 parser high-level functionality for parsing x509 certificates.
+CFG_ASN1_PARSER ?= y
+
 # Define the number of cores per cluster used in calculating core position.
 # The cluster number is shifted by this value and added to the core ID,
 # so its value represents log2(cores/cluster).
